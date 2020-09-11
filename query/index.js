4eae3a97982d0e5fbdd45a2a14eb25998fd063df
aeonincode <khashayar.sasannia@hotmail.com>
2020-09-11 09:03:57 +0200

    receiving events, creating the data query, parsing incoming event, install concurrently in root project

---
+++ query/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+// QUICK EXAMPLE
+// posts === {
+//     'jabsh556': {
+//         id: 'jabsh556',
+//         title: 'post title',
+//         comments: [
+//             { id: 'klj3kl', content: 'comment!' }
+//         ]
+//     },
+//     'jabsh556': {
+//         id: 'jabsh556',
+//         title: 'post title',
+//         comments: [
+//             { id: 'klj3kl', content: 'comment!' }
+//         ]
+//     },
+//     'jabsh556': {
+//         id: 'jabsh556',
+//         title: 'post title',
+//         comments: [
+//             { id: 'klj3kl', content: 'comment!' }
+//         ]
+//     },
+// }
+
+app.get('/posts', (req, res) => {
+  // when some1 ask for collection of all posts we have,we send back entire post object
+  res.send(posts);
+});
+
+app.post('events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('Listening on 4002');
+});
