13a4f0594a168afd3e7a60a4ed69bca29a7772aa
aeonincode <khashayar.sasannia@hotmail.com>
2020-09-12 13:33:35 +0200

    fix bug in query, handle resource update

--- query/index.js
+++ query/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+
 // QUICK EXAMPLE
 // posts === {
 //     'jabsh556': {
@@ -32,21 +33,20 @@ const posts = {};
 //     },
 // }
 
-app.get('/posts', (req, res) => {
-  // when some1 ask for collection of all posts we have,we send back entire post object
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('events', (req, res) => {
+app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  if (type === 'PostCreated') {
+  if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'CommentCreated') {
+  if (type === "CommentCreated") {
     const { id, content, postId } = data;
 
     const post = posts[postId];
@@ -59,5 +59,5 @@ app.post('events', (req, res) => {
 });
 
 app.listen(4002, () => {
-  console.log('Listening on 4002');
+  console.log("Listening on 4002");
 });
